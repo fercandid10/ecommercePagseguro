345719617541ae18bd0b71463f238dccd0add507
theflash1409 <theflash1409@gmail.com>
2021-09-20 17:13:23 -0300

    Opcoes de parcelamento

--- site-pagseguro.php
+++ site-pagseguro.php
@@ -33,7 +33,9 @@ $app->get('/payment', function(){
 		"years"=>$years,
 		"pagseguro"=>[
 			"urlJS"=>Config::getUrlJS(),
-			"id"=>Transporter::createSession()
+			"id"=>Transporter::createSession(),
+			" maxInstallmentNoInterest"=>Config::MAX_INSTALLMENT_NO_INTEREST,
+			" maxInstallment"=>Config::MAX_INSTALLMENT
 		]
 	]);
 });
