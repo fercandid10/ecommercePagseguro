d3b042babe1dd0816c97ce27d5d81e599cd95857
theflash1409 <theflash1409@gmail.com>
2021-09-22 18:06:59 -0300

    Classe Installment

--- vendor/hcodebr/php-classes/src/pagSeguro/CreditCard/Installment.php
+++ vendor/hcodebr/php-classes/src/pagSeguro/CreditCard/Installment.php
@@ -6,4 +6,22 @@ class Installment {
 
     private $quantity;
     private $value;
+
+    public function __construct(int $quantity, float $value){
+
+        if ($quantity < 1 || $quantity > Config::MAX_INSTALLMENT){
+
+            throw new Exception("Numero de parcelas invalidas");
+            
+        }
+
+        if ($value <= 0){
+
+            throw new Exception("Valor total invalido.");
+            
+        }
+
+        $this->quantity = $quantity;
+        $this->value = $value;
+    }
 }
