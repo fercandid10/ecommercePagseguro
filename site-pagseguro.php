1df54b59960fc49fab4210ebd9d5f3fb44fadd25
theflash1409 <theflash1409@gmail.com>
2021-09-24 14:56:04 -0300

    pagamento com debito

--- site-pagseguro.php
+++ site-pagseguro.php
@@ -16,6 +16,7 @@ use \Hcode\pagSeguro\Shipping;
 use \Hcode\pagSeguro\CreditCard\Installment;
 use \Hcode\pagSeguro\Item;
 use \Hcode\pagSeguro\Payment;
+use \Hcode\pagSeguro\Bank;
 
 $app->get("/payment/success/boleto", function(){
 
@@ -170,6 +171,66 @@ $app->post("/payment/boleto", function(){
 	}
 
 	$payment->setBoleto();
+
+	Transporter::sendTransaction($payment);
+
+	echo json_encode(['success'=>true]);
+
+
+});
+
+$app->post("/payment/debit", function(){
+
+	User::verifyLogin(false);
+
+	$order = new Order();
+	$order->getFromSession();
+	$order->get((int)$order->getidorder());
+	$address = $order->getAddress();
+
+	$cart = $order->getCart();
+
+	$cpf = new Document(Document::CPF, $_POST['cpf']);
+
+	$phone = new Phone($_POST['ddd'], $_POST['phone']);
+
+	$shippingAddress = new Address(
+		$address->getdesaddress(),
+		$address->getdesnumber(),
+		$address->getdescomplement(),
+		$address->getdesdistrict(),
+		$address->getdeszipcode(),
+		$address->getdescity(),
+		$address->getdesstate(),
+		$address->getdescountry()
+		
+	);
+
+	$birthDate = new DateTime($_POST['birth']);
+
+	$sender = new Sender($order->getdesperson(), $cpf, $birthDate, $phone, $order->getdesemail(), $_POST['hash'] );
+
+
+	$shipping = new Shipping($address, (float)$cart->getvlfreight(), Shipping::PAC);
+
+
+	$payment = new Payment($order->getidorder(), $sender, $shipping);
+
+	foreach($cart->getProducts() as $product){
+		
+		$item = new Item(
+			(int)$product['idproduct'],
+			$product['desproduct'],
+			(float)$product['vlprice'],
+			(int)$product['nrqtd']
+		);
+
+		$payment->addItem($item);
+	}
+
+	$bank = new Bank($_POST['bank']);
+
+	$payment->setBank($bank);
 
 	Transporter::sendTransaction($payment);
 
--- vendor/hcodebr/php-classes/src/pagSeguro/Bank.php
+++ vendor/hcodebr/php-classes/src/pagSeguro/Bank.php
@@ -2,11 +2,38 @@
 
 namespace Hcode\pagSeguro;
 
+use Exception;
+use DOMDocument;
+use DOMElement;
+
 
 class Bank {
 
     private $name;
     
+    public function __construct(string $name){
+
+        if (!$name){
+
+            throw new Exception("Informe o nome do banco..");
+            
+        }
+
+        $this->name = $name;
+    }
+
+    public function getDOMElement():DOMElement{
+
+        $dom = new DOMDocument();
+
+        $bank = $dom->createElement("bank");
+        $bank = $dom->appendChild($bank);
+
+        $name = $dom->createElement("name", $this->name);
+        $name = $dom->appendChild($name);
+
+        return $bank;
+    }
 
 
 }
